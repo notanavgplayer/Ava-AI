895c9e2f39e1273b3bc28ebd98db28837c5acf31
Asar Ahmed (Full Stack devleoper) <themernist@gmail.com>
2024-02-17 13:31:45 +0500

    fix host error

--- next.config.js
+++ next.config.js
@@ -1,21 +1,17 @@
 /** @type {import('next').NextConfig} */
 const withVideos = require("next-videos");
 
-module.exports = withVideos();
-const nextConfig = {
-    images: {
-      domains: [
-        "googleusercontent.com",
-        "oaidalleapiprodscus.blob.core.windows.net",
-        "cdn.openai.com",
-        "replicate.delivery",
-        "utfs.io"
-      ]
-
-    },
-  }
-  
-module.exports = nextConfig
-
-module.exports = withVideos();
-  
+module.exports = withVideos({
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "replicate.delivery",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+    ],
+  },
+});
